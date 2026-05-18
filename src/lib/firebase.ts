2af83ccf23148702f3de80b161d985b1ce8ae517
Thalibul Huda <hudathalibul41@gmail.com>
2026-05-18 18:55:05 +0700

    login google beres

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZle2HVFEV1cdO3ygLVYPnAQtC_o48UMU",
+  authDomain: "warung-camera.firebaseapp.com",
+  projectId: "warung-camera",
+  storageBucket: "warung-camera.firebasestorage.app",
+  messagingSenderId: "252383281119",
+  appId: "1:252383281119:web:5707e641e8796d0d458e6a",
+  measurementId: "G-GLJSRP3RME",
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
 
-// Connection test
-async function testConnection() {
-  try {
-    await getDocFromServer(doc(db, 'test', 'connection'));
-    console.log("Firebase connected successfully");
-  } catch (error) {
-    if (error instanceof Error && error.message.includes('the client is offline')) {
-      console.error("Please check your Firebase configuration.");
-    }
-  }
-}
-testConnection();
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
